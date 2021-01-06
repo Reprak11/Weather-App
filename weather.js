78d52633b0deb2da377b2ab1123f97f76dfb5f11
Reprak11 <reynaldo.armenta@cetys.edu.mx>
2021-01-05 18:56:05 -0800

    Estilos añadidos

--- weather.js
+++ weather.js
@@ -2,7 +2,8 @@ const components = {
     "myCity": document.getElementById("city"),
     "myTemp": document.getElementById("temp"),
     "inputCity": document.getElementById("cityInput"),
-    "weatherImg": document.getElementById("myImg")
+    "weatherImg": document.getElementById("myImg"),
+    "desc": document.getElementById("desc")
 }
 const button = document.getElementById("findCity"); 
 let datares;
@@ -18,8 +19,15 @@ button.addEventListener('click',
         (`https://api.openweathermap.org/data/2.5/weather?q=${components["inputCity"].value}&appid=${datares['key']}&units=metric`)
         let data = await response.json()
         console.log(data);
-        components["myCity"].innerHTML=data.name
-        components["myTemp"].innerHTML=Math.round(data.main.temp)+" °C"
-        components["weatherImg"].src=`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        components["myCity"].innerText=data.name;
+        components["myTemp"].innerText=Math.round(data.main.temp)+" °C";
+        components["weatherImg"].src=`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        components["desc"].innerText=data.weather[0].description;
+        components["inputCity"].value=""
+        if(data.weather[0].icon.includes("d")){
+            document.body.style.background= "linear-gradient(78deg, rgba(146,230,238,0.9868989832261029) 8%, rgba(87,223,232,1) 22%, rgba(92,185,232,1) 72%, rgba(0,212,255,1) 100%)";
+        } else {
+            document.body.style.background= "linear-gradient(337deg, rgba(13,174,207,1) 14%, rgba(0,122,131,1) 60%, rgba(0,25,37,1) 99%, rgba(0,0,0,1) 100%)";
+        }
     }
 )
