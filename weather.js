75d9be83d639d89c84de5036e1e7b9a3f50a5d9f
Reprak11 <reynaldo.armenta@cetys.edu.mx>
2021-01-06 21:25:29 -0800

    Enter key available

--- weather.js
+++ weather.js
@@ -13,21 +13,26 @@ window.onload = async () => {
     datares = await keyres.json();
 }
 
-button.addEventListener('click',
-    async () => {
-        let response = await fetch
-        (`https://api.openweathermap.org/data/2.5/weather?q=${components["inputCity"].value}&appid=${datares['key']}&units=metric`)
-        let data = await response.json()
-        console.log(data);
-        components["myCity"].innerText=data.name;
-        components["myTemp"].innerText=Math.round(data.main.temp)+" °C";
-        components["weatherImg"].src=`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-        components["desc"].innerText=data.weather[0].description;
-        components["inputCity"].value=""
-        if(data.weather[0].icon.includes("d")){
-            document.body.style.background= "linear-gradient(78deg, rgba(146,230,238,0.9868989832261029) 8%, rgba(87,223,232,1) 22%, rgba(92,185,232,1) 72%, rgba(0,212,255,1) 100%)";
-        } else {
-            document.body.style.background= "linear-gradient(337deg, rgba(13,174,207,1) 14%, rgba(0,122,131,1) 60%, rgba(0,25,37,1) 99%, rgba(0,0,0,1) 100%)";
-        }
+const getData = async () => {
+    let response = await fetch
+    (`https://api.openweathermap.org/data/2.5/weather?q=${components["inputCity"].value}&appid=${datares['key']}&units=metric`)
+    let data = await response.json()
+    console.log(data);
+    components["myCity"].innerText=data.name;
+    components["myTemp"].innerText=Math.round(data.main.temp)+" °C";
+    components["weatherImg"].src=`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    components["desc"].innerText=data.weather[0].description;
+    components["inputCity"].value=""
+    if(data.weather[0].icon.includes("d")){
+        document.body.style.background= "linear-gradient(78deg, rgba(146,230,238,0.9868989832261029) 8%, rgba(87,223,232,1) 22%, rgba(92,185,232,1) 72%, rgba(0,212,255,1) 100%)";
+    } else {
+        document.body.style.background= "linear-gradient(337deg, rgba(13,174,207,1) 14%, rgba(0,122,131,1) 60%, rgba(0,25,37,1) 99%, rgba(0,0,0,1) 100%)";
     }
-)
+}
+
+components.inputCity.addEventListener('keypress', e =>{
+    if (e.key === 'Enter') {
+        getData()
+    }
+})
+button.addEventListener('click', getData);
